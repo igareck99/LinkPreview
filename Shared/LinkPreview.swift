a2fef102d4fecede673f7704400f97dfa529108c
Igor <igor9.i@yandex.ru>
2021-12-11 05:47:48 +0300

    create

---
+++ Shared/LinkPreview.swift
@@ -0,0 +1,8 @@
+//
+//  LinkPreview.swift
+//  LinkPreview (iOS)
+//
+//  Created by Игорь Коноваленко on 11.12.2021.
+//
+
+import Foundation
---
+++ Shared/ViewModel/MessageViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MessageViewModel.swift
+//  LinkPreview (iOS)
+//
+//  Created by Игорь Коноваленко on 11.12.2021.
+//
+
+import Foundation
