b6cf0becb9770f4062d594f3046bdc139e787416
Igor <igor9.i@yandex.ru>
2021-12-11 05:32:30 +0300

    Initial Commit

---
+++ Shared/LinkPreviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkPreviewApp.swift
+//  Shared
+//
+//  Created by Игорь Коноваленко on 11.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct LinkPreviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
